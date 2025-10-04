34648b781865ea02e8c99a8ad86376e527f3c729
diaa <peachdiaa@gmail.com>
2025-10-04 22:00:38 +0300

    responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
 import "./NavBar.css";
+import { useState } from "react";
 import { Menu, X } from "lucide-react";
 
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <nav>
@@ -18,6 +23,23 @@ export default function NavBar() {
               <li>Projects</li>
               <li>Contact</li>
             </ul>
+          </div>
+          <div className="mobile-right-section">
+            <div className="lucuide-button" onClick={toggleMenuOpen}>
+              {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
+            </div>
+            {isMenuOpen ? (
+              <div className="nav-menu">
+                <ul>
+                  <li onClick={toggleMenuOpen}>Home</li>
+                  <li onClick={toggleMenuOpen}>About</li>
+                  <li onClick={toggleMenuOpen}>Projects</li>
+                  <li onClick={toggleMenuOpen}>Contact</li>
+                </ul>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </nav>
