ddd4aa494ca2880b0415dc795156416cffb2d666
diaa <peachdiaa@gmail.com>
2025-10-07 21:44:45 +0300

    inhanced navbar ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+function setVh() {
+  document.documentElement.style.setProperty(
+    "--vh",
+    `${window.innerHeight * 0.01}px`
+  );
+}
+setVh();
+let vhResizeTimeout;
+window.addEventListener("resize", () => {
+  clearTimeout(vhResizeTimeout);
+  vhResizeTimeout = setTimeout(setVh, 150);
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
